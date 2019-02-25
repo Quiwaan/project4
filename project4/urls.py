4640e6ef5feb327d84c5708e564e550ba46f509e
quiwaan <quiwaan@me.com>
2019-02-25 15:38:10 -0800

    added ask-question route

--- project4/urls.py
+++ project4/urls.py
@@ -19,5 +19,5 @@ from qa import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index, name='index')
 ]
--- qa/views.py
+++ qa/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {}
+    context['questions'] = Question.objects.all()
+    return render(request, 'index.html', context)
+
+def askquestion(request):
+    if request.method == 'POST':
+        try: 
+            q = Question(question_title=title, question_text=question, posted_by=posted_by)
+            title = request.POST.get('title')
+            question = request.POST.get('question')
+            posted_by = request.POST.get('posted_by')
+            q.save()
+            return redirect(viewquestion, q.qid, q.slug)
+        except:
+            return render(request, 'ask-question.html', {'error': 'something wrong with form'})
+    else:
+        return render(request, 'ask-question.html', {} )
